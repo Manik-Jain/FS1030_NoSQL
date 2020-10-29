1538bc725d003123eeb55b8a1570a3eb0fde0f75
Manik Jain <jain.manik@yahoo.co.in>
2020-10-29 19:59:38 +0100

    removing the exposed URL

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    //url: 'mongodb://localhost:27017/easy-notes'
-    url : 'mongodb+srv://root:root@cluster0.qyyno.mongodb.net/Notes?retryWrites=true&w=majority'
+    url: 'mongodb://localhost:27017/easy-notes'
 }
